ea78943e38c5a8215454f4b1e2d456f445438c74
vancanhuit <vancanhuit@gmail.com>
2021-03-28 10:20:22 +0700

    Use jenkins version 2.263.4

--- src/test/groovy/demo/SampleJobSpec.groovy
+++ src/test/groovy/demo/SampleJobSpec.groovy
@@ -1,19 +1,24 @@
 package demo
 
 import javaposse.jobdsl.dsl.DslScriptLoader
-import javaposse.jobdsl.dsl.FileJobManagement
+import javaposse.jobdsl.plugin.JenkinsJobManagement
+import org.junit.Rule
+import org.jvnet.hudson.test.JenkinsRule
 import spock.lang.Specification
 
 class SampleJobSpec extends Specification {
+    @Rule
+    JenkinsRule jenkinsRule = new JenkinsRule()
+
     void 'test sample job'() {
         given:
-        FileJobManagement fileJobManagement = new FileJobManagement(new File('.'))
+        JenkinsJobManagement jobManagement = new JenkinsJobManagement(System.out, [:], new File('.'))
         String script = '''import demo.JobBuilder
 def job = freeStyleJob('SampleJob')
 JobBuilder builder = new JobBuilder(job)
 builder.addDescription('Sample Job')'''
         when:
-        new DslScriptLoader(fileJobManagement).runScript(script)
+        new DslScriptLoader(jobManagement).runScript(script)
 
         then:
         noExceptionThrown()
