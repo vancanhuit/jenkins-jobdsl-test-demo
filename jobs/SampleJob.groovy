757859bd0da61f3ddc69fbb48a09e93148836976
vancanhuit <vancanhuit@gmail.com>
2021-04-03 11:08:24 +0700

    Refactor

--- jobs/SampleJob.groovy
+++ jobs/SampleJob.groovy
@@ -1,6 +1,7 @@
 import demo.JobBuilder
+import javaposse.jobdsl.dsl.jobs.FreeStyleJob
 
-def job = freeStyleJob('SampleJob')
+FreeStyleJob job = freeStyleJob('SampleJob')
 JobBuilder builder = new JobBuilder(job)
 builder.setDescription('SampleJob')
 builder.setConcurrentBuild(true)
